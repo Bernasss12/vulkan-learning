fddd27fc0dd1160a34797fdf48f05c5cc83e11d3
Bernardo Antunes <bernasss12@gmail.com>
2023-11-18 00:30:27 +0000

    Create PresentQueue

--- src/main/kotlin/dev/bernasss12/vklearn/engine/graphics/vulkan/Queue.kt
+++ src/main/kotlin/dev/bernasss12/vklearn/engine/graphics/vulkan/Queue.kt
@@ -6,25 +6,29 @@
 
 package dev.bernasss12.vklearn.engine.graphics.vulkan
 
+import dev.bernasss12.vklearn.util.VulkanUtils.useMemoryStack
+import dev.bernasss12.vklearn.util.VulkanUtils.vkCreateInt
+import dev.bernasss12.vklearn.util.VulkanUtils.vkCreatePointer
 import org.lwjgl.system.MemoryStack
+import org.lwjgl.vulkan.KHRSurface
 import org.lwjgl.vulkan.VK10.*
 import org.lwjgl.vulkan.VkQueue
 import org.tinylog.kotlin.Logger
 
 open class Queue(
     device: Device,
-    queueFamilyIndex: Int,
+    val queueFamilyIndex: Int,
     queueIndex: Int,
 ) {
-    val vkQueue: VkQueue
+    private val vkQueue: VkQueue
 
     init {
         Logger.debug("Creating queue")
 
-        MemoryStack.stackPush().use { stack ->
-            val pQueue = stack.mallocPointer(1)
-            vkGetDeviceQueue(device.vkDevice, queueFamilyIndex, queueIndex, pQueue)
-            val queue = pQueue[0]
+        useMemoryStack { stack ->
+            val queue = stack.vkCreatePointer { buffer ->
+                vkGetDeviceQueue(device.vkDevice, queueFamilyIndex, queueIndex, buffer)
+            }
             vkQueue = VkQueue(queue, device.vkDevice)
         }
     }
@@ -33,14 +37,52 @@ open class Queue(
         vkQueueWaitIdle(vkQueue)
     }
 
-    class GraphicsQueue(device: Device, queueIndex: Int) : Queue(device, getGraphicsQueueFamilyIndex(device), queueIndex) {
+    class GraphicsQueue(
+        device: Device,
+        queueIndex: Int
+    ) : Queue(
+        device,
+        getGraphicsQueueFamilyIndex(device),
+        queueIndex,
+    ) {
         companion object {
             private fun getGraphicsQueueFamilyIndex(device: Device): Int {
                 return device.physicalDevice.vkQueueFamilyProperties.indexOfFirst { queueFamilyProperty ->
                     queueFamilyProperty.queueFlags() and VK_QUEUE_GRAPHICS_BIT != 0
                 }.also { index ->
                     if (index < 0) {
-                        throw RuntimeException("Failed to get graphics Queue family index")
+                        throw RuntimeException("Failed to get Graphics Queue family index")
+                    }
+                }
+            }
+        }
+    }
+
+    class PresentQueue(
+        device: Device,
+        surface: Surface,
+        queueIndex: Int,
+    ): Queue(
+        device,
+        getPresentQueuFamilyIndex(device, surface),
+        queueIndex,
+    ) {
+        companion object {
+            private fun getPresentQueuFamilyIndex(device: Device, surface: Surface): Int {
+                useMemoryStack { stack ->
+                    return device.physicalDevice.vkQueueFamilyProperties.withIndex().indexOfFirst { indexedVkQueueFamilyProperty ->
+                        stack.vkCreateInt { buffer ->
+                            KHRSurface.vkGetPhysicalDeviceSurfaceSupportKHR(
+                                device.physicalDevice.vkPhysicalDevice,
+                                indexedVkQueueFamilyProperty.index,
+                                surface.vkSurface,
+                                buffer,
+                            )
+                        } == VK_TRUE
+                    }.also { index ->
+                        if (index < 0) {
+                            throw RuntimeException("Failed to get Presentation Queue family index")
+                        }
                     }
                 }
             }

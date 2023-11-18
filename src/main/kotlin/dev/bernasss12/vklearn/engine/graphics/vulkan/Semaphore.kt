7c50e162dacf13ee46fd2b1e0b91534d111cb79a
Bernardo Antunes <bernasss12@gmail.com>
2023-11-18 00:29:08 +0000

    Create Semaphore

---
+++ src/main/kotlin/dev/bernasss12/vklearn/engine/graphics/vulkan/Semaphore.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2023 Bernardo Antunes
+ *
+ * For the purpose of not bloating every single file in the project, refer to the version of the MIT license provided in the project in `LICENCE.md`
+ */
+
+package dev.bernasss12.vklearn.engine.graphics.vulkan
+
+import dev.bernasss12.vklearn.util.VulkanUtils.vkAssertSuccess
+import dev.bernasss12.vklearn.util.VulkanUtils.vkCreateInt
+import org.lwjgl.system.MemoryStack
+import org.lwjgl.vulkan.VK10.*
+import org.lwjgl.vulkan.VkSemaphoreCreateInfo
+
+class Semaphore(
+    private val device: Device
+) {
+    val vkSemaphore: Long
+
+    init {
+        MemoryStack.stackPush().use { stack ->
+            val semaphoreCreateInfoBuffer = VkSemaphoreCreateInfo.calloc(stack).apply {
+                sType(VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO)
+            }
+
+            //TODO check if this works
+            vkSemaphore = stack.vkCreateInt { buffer ->
+                vkCreateSemaphore(
+                    device.vkDevice,
+                    semaphoreCreateInfoBuffer,
+                    null,
+                    buffer
+                ).vkAssertSuccess("Failed to create semaphore")
+            }
+        }
+    }
+
+    fun cleanup() {
+        vkDestroySemaphore(
+            device.vkDevice,
+            vkSemaphore,
+            null,
+        )
+    }
+}
